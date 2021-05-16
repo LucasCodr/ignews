194afab2f1f445da0b59eab7d6cc46813761e977
Lucas <lucassouza876@outlook.com>
2021-05-16 19:49:32 -0300

    Criaçao do header da aplicaçao

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,17 @@
 import { AppProps } from 'next/app'
+import { Header } from '../components/Header'
 import '../styles/global.scss'
 // Esse componente está presente em todas as paginas
 // Sempre é carregado quando o usuario troca de pagina
 // Nunca devemos utilizar o _app para realizar ações que devem ser executadas apenas 1 vez (ex: configuracao de fontes externas)
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
