96884b241cd2c7e55cd180bb110761bc7417cf8a
Lucas <lucassouza876@outlook.com>
2022-02-06 10:56:43 -0300

    Adding stripe config

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,17 @@
 import Head from 'next/head'
 import { SubscribeButton } from '../components/SubscribeButton'
 import styles from './home.module.scss'
+import {GetServerSideProps, GetStaticProps} from "next";
+import {stripe} from "../services/stripe";
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: string;
+  }
+}
+
+export default function Home({product}: HomeProps) {
   return (
     <>
       <Head>
@@ -15,13 +24,32 @@ export default function Home() {
           <h1>News about the <span>React</span> world.</h1>
           <p>
             Get access to all publications <br />
-            <span>for $9.90/month</span>
+            <span>for {product.amount}/month</span>
           </p>
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId}/>
         </section>
 
         <img src="/images/avatar.svg" alt="Girl Coding" />
       </main>
     </>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve('price_1IrtxKJWc3Xai84Vi6KJW2DO');
+  
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', { 
+      currency: 'USD',
+      style: 'currency'
+    }).format(price.unit_amount / 100),
+  }; 
+  
+  return {
+    props: {
+      product,
+    },
+    revalidate: 60 * 60 * 24 // 24 hours
+  }
 }
