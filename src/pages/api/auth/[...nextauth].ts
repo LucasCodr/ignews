73ad343e247b0dc1759d6bc7a6aad277bbdf69be
Lucas <lucassouza876@outlook.com>
2022-03-27 14:26:17 -0300

    Adding subscribe functionality with stripe

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+import {query as q} from 'faunadb'
+import {fauna} from '../../../services/fauna';
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -9,4 +11,42 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_SECRET,
     }),
   ],
+  callbacks: {
+    async signIn({ user }) {
+
+      const {email} = user;
+
+      try {
+
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email } }
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(email)
+              )
+            )
+          )
+        )
+        
+      } catch (e) {       
+
+         return false;
+      }      
+
+      return true;
+    }
+  }
 })
