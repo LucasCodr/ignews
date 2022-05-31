17c3f03338907bcacd359882f04b1f77d78e9bbc
Lucas Souza <lucasknot1@gmail.com>
2022-05-31 12:23:34 +0000

    2.0

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,7 +6,7 @@ export function Header() {
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
-        <img src="/images/logo.svg" alt="ig.news" />
+        <img src="/images/logo.svg" alt="ig.news 2.0" />
 
         <nav>
           <a href="#" className={styles.active}>Home</a>
